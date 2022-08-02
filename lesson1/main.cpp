3040cf3c4d54723cbec6e180007a476b7bf7f0f5
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-08-02 23:01:38 +0800

    set up env for programming opengl

---
+++ lesson1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+const char* APP_TITLE = "Introduction to Modern OpenGL - Hello Window 1";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW initialization failed" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+    if (pWindow == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(pWindow);
+
+    glfwTerminate();
+    return 0;
+}
