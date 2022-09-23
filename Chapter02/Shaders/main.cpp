f792790239f9ce821aa41d10fdbee278767457a1
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-09-23 12:12:29 +0800

    use one vec3 instead

--- Chapter02/Shaders/main.cpp
+++ Chapter02/Shaders/main.cpp
@@ -17,7 +17,7 @@ const GLchar* vertexShaderSource = "#version 330 core\n"
 "out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"gl_Position = vec4(position, 1.0f);\n"
 "ourColor = color;\n"
 "}\0";
 const GLchar* fragmentShaderSource = "#version 330 core\n"
